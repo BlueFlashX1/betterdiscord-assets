24c557b9584c53c2fdac143a37e9a2ae2bafbf29
Matthew Thompson <matthew.thompson@example.com>
2026-02-17 20:04:55 -0700

    style(toolbar): shift buttons -4px left

--- plugins/SkillTree.plugin.js
+++ plugins/SkillTree.plugin.js
@@ -2279,7 +2279,7 @@ module.exports = class SkillTree {
         align-items: center;
         justify-content: center;
         padding: 0;
-        margin: 0 1px 0 5px;
+        margin: 0 1px 0 -4px;
         box-sizing: border-box;
       }
       .st-skill-tree-button {
