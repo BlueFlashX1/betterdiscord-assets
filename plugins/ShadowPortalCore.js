850a025df09e8371fae8e1cb339415e2b4add06d
Matthew Thompson <matthew.thompson@example.com>
2026-02-27 23:38:00 -0700

    fix(portal): use self-hosted portal_mask2.png instead of imgur
    Switch spiral mask URL from imgur to raw.githubusercontent.com
    hosted from this repo's themes/animation_mask/portal_mask2.png.

--- plugins/ShadowPortalCore.js
+++ plugins/ShadowPortalCore.js
@@ -18,7 +18,7 @@ let _gsapLoaded = false;
 let _spiralMaskUrl = null;
 let _spiralMaskReady = false;
 
-const SPIRAL_IMG_URL = "https://i.imgur.com/oNjtnOI.png";
+const SPIRAL_IMG_URL = "https://raw.githubusercontent.com/BlueFlashX1/betterdiscord-assets/main/themes/animation_mask/portal_mask2.png";
 
 /**
  * Preload the spiral mask image from imgur.
